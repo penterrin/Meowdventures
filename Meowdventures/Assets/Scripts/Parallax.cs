4fb1494bc91b158fd6ddc1ef8f118ceda9ef8562
informaticaesne <penterrin@gmail.com>
2024-02-06 13:33:24 +0100

    Parallax
    requiere arreglos
    06/02/2024
    1h20

--- Meowdventures/Assets/Scripts/Parallax.cs
+++ Meowdventures/Assets/Scripts/Parallax.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     private float lenght, startpos;
-        public GameObject cam;
+    public GameObject cam;
     public float parallaxEffect;
 
     // Start is called before the first frame update
     void Start()
     {
+        startpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float temp = (cam.transform.position.x * (1 -parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos +dist, transform.position.y, transform.position.z);
+
+        if(temp > startpos + lenght) startpos += temp;
+        else if (temp < startpos - lenght) startpos -= temp;
     }
 }
